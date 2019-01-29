1f26739f8bcd381d0cde59d22c44b6a31fde41f2
Kubilay Yazi <kubilayyazi@hotmail.com>
2019-01-29 11:22:31 +0300

    controller oluşturuldu

---
+++ Admin.Web.UI/Controllers/PartialController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Admin.Web.UI.Controllers
+{
+    public class PartialController : Controller
+    {
+        public PartialViewResult DrawerPartial()
+        {
+            var data = new List<string>();
+            return PartialView("Partials/_DrawerPartial", data);
+        }
+        public PartialViewResult HeaderPartial()
+        {
+            return PartialView("Partials/_HeaderPartial");
+        }
+
+
+    }
+}
