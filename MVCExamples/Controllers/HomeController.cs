2cd99eb2fd13bdf43d8e948415056957188c107a
Kubilay Yazi <kubilayyazi@hotmail.com>
2019-01-29 09:29:43 +0300

    Add project files.

---
+++ MVCExamples/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCExamples.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Hakkimizda()
+        {
+            return View();
+        }
+        public ActionResult Iletisim()
+        {
+            return View();
+        }
+
+
+        public PartialViewResult UrunGetir()
+        {
+            List<string> urunListesi = new List<string>();
+            urunListesi.Add("kalem");
+            urunListesi.Add("silgi  ");
+            urunListesi.Add("defter");
+            return PartialView("~/Views/Home/_partialUrunGetir.cshtml", urunListesi);
+
+        }
+    }
+}
---
+++ WebSiteProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebSiteProject.MvcApplication" Language="C#" %>
