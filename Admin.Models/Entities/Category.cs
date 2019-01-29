ab9a16129a7ad1361b57514c99199a9bbeae657f
Kubilay Yazi <kubilayyazi@hotmail.com>
2019-01-29 14:25:05 +0300

    codefirst tamamlandı

---
+++ Admin.Models/Entities/Category.cs
@@ -0,0 +1,32 @@
+﻿using Admin.Models.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admin.Models.Entities
+{[Table("Categories")]
+   public class Category:BaseEntity<int>
+    {
+
+        [StringLength(100,ErrorMessage ="Kategori adı 3 ile  100 arasında karakter olabilir",MinimumLength =3)]
+        [DisplayName("Kategori Adı")]
+        [Required]
+        public string CategoryName { get; set; }
+
+        [Range(0, 1)]
+        public decimal TaxRate { get; set; } = 0;
+
+        public int? SupCategoryId { get; set; }
+
+        [ForeignKey("SupCategory")]
+        public virtual Category SupCategory{ get; set; }
+        public virtual ICollection<Category> Categories { get; set; } = new HashSet<Category>();
+
+        public virtual ICollection<Product> Products { get; set; } = new HashSet<Product>();
+    }
+}
---
+++ Admin.Models/Entities/Product.cs
@@ -0,0 +1,60 @@
+﻿using Admin.Models.Abstracts;
+using Admin.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admin.Models.Entities
+{
+    [Table("Products")]
+   public class Product :BaseEntity<Guid>
+    {
+        public Product()
+        {
+            this.Id = Guid.NewGuid();
+        }
+
+        [StringLength(100, ErrorMessage = "ürün adı 1 ile  100 arasında karakter olabilir", MinimumLength = 1)]
+        [DisplayName("Ürün Adı")]
+        [Required]
+        public string ProductName { get; set; }
+        [DisplayName("Ürün Adı")]
+
+        public ProductTypes ProductTypes  { get; set; }
+        [DisplayName("Satış Fiyatı")]
+        public decimal SalesPrice { get; set; }
+        [DisplayName("Alış Fiyatı")]
+        public decimal BuyPrice { get; set; }
+        [DisplayName("Stok Miktarı")]
+        [Range(0,9999)]
+        public double UnitInStock { get; set; }
+
+        public DateTime LastPriceUpdateDate { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public Guid? SupProductId { get; set; }
+        [StringLength(20)]
+        [Required]
+        [Index(IsUnique =true)]
+        public string Barcode { get; set; }
+        [DisplayName("Birim")]
+        public int Quantity { get; set; }
+        [DisplayName("Açıklama")]
+        public string Description { get; set; }
+        [ForeignKey("CategoryId")]
+        public virtual Category  Category { get; set; }
+        [ForeignKey("SupProductId")]
+        public virtual Product SupProduct { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; } = new HashSet<Product>();
+
+        public virtual ICollection<Invoice> Invoices { get; set; } = new HashSet<Invoice>();
+
+    }
+}
