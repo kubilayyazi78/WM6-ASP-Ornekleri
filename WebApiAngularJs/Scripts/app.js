dd12646a0e7149086b3cb031a32f215fab410cd6
kubilay yazı <kubilayyazi@hotmail.com>
2019-02-28 23:22:54 +0300

    sepete ekleme işlemi tamamlandı.

--- WebApiAngularJs/Scripts/app.js
+++ WebApiAngularJs/Scripts/app.js
@@ -3,10 +3,16 @@ var app = angular.module("myApp", []);
 
 app.controller("ProductCtrl", function ($scope) {
     $scope.urunler = [];
+    $scope.sepetList = [];
+    $scope.ekleMi = false;
+    $scope.sepetToplam = 0;
 
     function init() {
         var data = JSON.parse(localStorage.getItem("urunler"));
         $scope.urunler = data === null ? [] : data;
+        data = JSON.parse(localStorage.getItem("sepet"));
+        $scope.sepetList = data === null ? [] : data;
+        sepetHesapla();
     }
     $scope.ekle = function () {
         $scope.urunler.push({
@@ -23,12 +29,46 @@ app.controller("ProductCtrl", function ($scope) {
     $scope.sil = function (id) {
         for (var i = 0; i < $scope.urunler.length; i++) {
             var data = $scope.urunler[i];
-            if (id == data.id) {
+            if (id === data.id) {
                 $scope.urunler.splice(i, 1);
                 break;
             }
         }
         localStorage.setItem("urunler", JSON.stringify($scope.urunler));
+    };
+
+    $scope.sepeteekle = function (urun) {
+        var varMi = false;
+        for (var i = 0; i < $scope.sepetList.length; i++) {
+            var data = $scope.sepetList[i];
+            if (data.id === urun.id) {
+                varMi = true;
+                data.adet++;
+                break;
+            }
+        }
+        if (!varMi) {
+            urun.adet = 1;
+            $scope.sepetList.push(urun);
+        }
+        localStorage.setItem("sepet", JSON.stringify($scope.sepetList));
+        sepetHesapla();
+    };
+
+    function sepetHesapla() {
+        $scope.sepetToplam = 0;
+        for (var i = 0; i < $scope.sepetList.length; i++) {
+            var data = $scope.sepetList[i];
+            $scope.sepetToplam += data.adet * data.fiyat;
+        }
+    }
+
+    $scope.cikart = function (urun) {
+        var index = $scope.sepetList.indexOf(urun);
+        if (index > -1)
+            $scope.sepetList.splice(index, 1);
+        localStorage.setItem("sepet", JSON.stringify($scope.sepetList));
+        sepetHesapla();
     };
 
     function guid() {
