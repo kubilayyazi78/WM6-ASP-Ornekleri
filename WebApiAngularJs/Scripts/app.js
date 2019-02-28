381fba45d1d9e52bfad11a451d11bf77f1851f02
Kubilay Yazi <kubilayyazi@hotmail.com>
2019-02-28 08:47:23 +0300

    angularjs bug fixed ok

--- WebApiAngularJs/Scripts/app.js
+++ WebApiAngularJs/Scripts/app.js
@@ -1,9 +1,7 @@
 ﻿/// <reference path="angular.js" />
 var app = angular.module("myApp", []);
 
-
 app.controller("ProductCtrl", function ($scope) {
-
     $scope.urunler = [];
 
     function init() {
@@ -11,20 +9,25 @@ app.controller("ProductCtrl", function ($scope) {
         $scope.urunler = data === null ? [] : data;
     }
     $scope.ekle = function () {
-
         $scope.urunler.push({
+            id: guid(),
             urunAdi: $scope.yeni.urunAdi,
             fiyat: $scope.yeni.fiyat,
             eklenmeZamani: new Date()
-
         });
         $scope.yeni.urunAdi = "";
-        $scope.yeni.fiyat = 0;
+        $scope.yeni.fiyat = "";
         localStorage.setItem("urunler", JSON.stringify($scope.urunler));
-        init();
     };
 
-}
+    function guid() {
+        function S4() {
+            return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+        }
 
-    
-);
+        // then to call it, plus stitch in '4' in the third group
+        return (S4() + S4() + "-" + S4() + "-4" + S4().substr(0, 3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase();
+    }
+
+    init();
+});
