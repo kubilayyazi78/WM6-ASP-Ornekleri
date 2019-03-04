e6152c681d54e94b411158f6e13a370b08df7e57
kubilay yazı <kubilayyazi@hotmail.com>
2019-03-04 19:34:25 +0300

    SignalR Sohbet uygulaması tamamlandı.

---
+++ SingalRApp/ChatHub.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SingalRApp
+{
+    public class ChatHub : Hub
+    {
+        public static List<User> UserList = new List<User>();
+        public void HerkeseGonder(string gonderen, string mesaj)
+        {
+            Clients.All.herkeseGonder(gonderen, mesaj, $"{DateTime.Now:g}");
+        }
+
+        public void OzelMesaj(string gonderenId, string aliciId, string mesaj)
+        {
+            Clients.Client(aliciId).mesajGeldi(gonderenId, mesaj);
+        }
+
+        public void Login(string kullaniciAdi, string id)
+        {
+            var giris = UserList.FirstOrDefault(x => x.Id == id);
+            giris.UserName = kullaniciAdi;
+
+            Clients.All.users(UserList);
+        }
+
+        public override Task OnConnected()
+        {
+            UserList.Add(new User()
+            {
+                Id = Context.ConnectionId
+            });
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            var silinecek = UserList.FirstOrDefault(x => x.Id == Context.ConnectionId);
+            UserList.Remove(silinecek);
+            Clients.All.users(UserList);
+            return base.OnDisconnected(stopCalled);
+        }
+    }
+
+    public class User
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+    }
+}
