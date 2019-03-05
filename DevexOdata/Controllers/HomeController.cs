fe5717500e49800c69649bc276ee17e766df4007
Kubilay Yazi <kubilayyazi@hotmail.com>
2019-03-05 09:57:58 +0300

    Fakedata ile veriler oluşturuldu.

--- DevexOdata/Controllers/HomeController.cs
+++ DevexOdata/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DevexOdata.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,7 +17,24 @@ namespace DevexOdata.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
+            ApplicationDbContext db = new ApplicationDbContext();
+            for (int i = 0; i < 5000; i++)
+            {
+                db.Customers.Add(new Customer()
+                {
+                    Name = FakeData.NameData.GetFirstName(),
+                    Adress = FakeData.PlaceData.GetAddress(),
+                    Balance = FakeData.NumberData.GetNumber(1250, 99999),
+                    Surname = FakeData.NameData.GetSurname(),
+                    Phone = "05" + FakeData.PhoneNumberData.GetPhoneNumber().Replace("-", "").Substring(0, 10)
+                });
+                if (i % 100 == 0)
+                    {
+                    db.SaveChanges();
+                    }
 
+            }
+            db.SaveChanges();
             return View();
         }
 
